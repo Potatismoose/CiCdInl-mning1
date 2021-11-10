c16a76c5c2ea06f2337cb0ea1c959222da166259
Benny Christensen <benny@potatismoose.com>
2021-11-10 09:38:25 +0100

    Login done and tests completed.

--- CiCdInlämning1/Models/Users/Account.cs
+++ CiCdInlämning1/Models/Users/Account.cs
@@ -20,5 +20,10 @@ namespace CiCdInlämning1.Models.Users
         public string Password { get; set; }
         public string Email { get; set; }
         public bool IsAdmin { get; set; }
+
+        public override string ToString()
+        {
+            return ($"{Name}, Har adminbehörighet: {IsAdmin}");
+        }
     }
 }
--- CiCdInlämning1/Program.cs
+++ CiCdInlämning1/Program.cs
@@ -1,16 +1,9 @@
-﻿using CiCdInlämning1.Models.Users;
-using System;
-
-namespace CiCdInlämning1
+﻿namespace CiCdInlämning1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            User user = new(1, "John Doe", "Testar123", "testarigen@test.com");
-            Admin admin = new(1, "John Doe", "Testar123", "testarigen@test.com");
-            Console.WriteLine(admin.IsAdmin);
-            Console.ReadKey();
         }
     }
 }
