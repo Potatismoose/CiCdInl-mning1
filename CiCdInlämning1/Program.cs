8f63b992af3a6560aeaeec56b2d7625b396abfa2
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-11-10 16:18:39 +0100

    Get List of users Tests and method done. Serialize and deserialize methods done.

--- CiCdInlämning1/Program.cs
+++ CiCdInlämning1/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace CiCdInlämning1
+﻿using CiCdInlämning1.Models.Users;
+using CiCdInlämning1.Utilities;
+using System;
+
+namespace CiCdInlämning1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ReadWrite.Deserialize();
+            
+            ReadWrite.Serialize(new User(1, "Johan", "testar123", "bad@bad.com"));
+            foreach (var item in ReadWrite.GetListOfUsers())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+            // Det första som skall ske är en deserialize för att kolla om filerna finns.
+            // Sen kör vi get list of users, om dess count är 0. Skapa en ny admin som skickas in 
+            // i serialize samt två st dummy users.  
+            // Sen kör vi deserialize igen för att spara in admin i listan.
+            // lägg detta i en do while loop som kontrollerar om getList of users är större än 0
+            // är den det så avsluta loopen och gå till inloggnings GUI.
+            // 
+
         }
     }
 }
---
+++ CiCdInlämning1Tests/Utilities/ReadWriteTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using CiCdInlämning1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CiCdInlämning1.Models.Users;
+using CiCdInlämning1.Interfaces;
+
+namespace CiCdInlämning1.Utilities.Tests
+{
+    [TestFixture()]
+    public class ReadWriteTests
+    {
+        [Test()]
+        public void GetListOfUsers_Is_not_Null()
+        {
+            // Act
+            ReadWrite.Serialize(new User(1, "Johan", "testar123", "bad@bad.com"));
+            ReadWrite.Deserialize();
+            // Arrange
+            var actual = ReadWrite.GetListOfUsers();
+            // Assert
+            Assert.That(actual, Is.Not.Null);
+        }
+    }
+}
