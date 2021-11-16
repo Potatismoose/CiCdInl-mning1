4775574dc80377239a3b61b2a493ecce7d370540
Benny Christensen <benny@potatismoose.com>
2021-11-16 10:56:54 +0100

    Updated

--- CiCdInlämning1Tests/Utilities/ReadWriteTests.cs
+++ CiCdInlämning1Tests/Utilities/ReadWriteTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CiCdInlämning1.Models.Users;
 
 namespace CiCdInlämning1.Utilities.Tests
 {
