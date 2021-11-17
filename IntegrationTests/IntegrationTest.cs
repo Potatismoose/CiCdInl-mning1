dfcaecaa7fd7528ddf5365ec87f37047f0a48dc0
Benny Christensen <benny@potatismoose.com>
2021-11-17 16:07:00 +0100

    Test

--- IntegrationTests/IntegrationTest.cs
+++ IntegrationTests/IntegrationTest.cs
@@ -1,40 +1,40 @@
-using CiCdInlämning1.Controllers;
-using CiCdInlämning1.Models.Users;
-using CiCdInlämning1.Utilities;
-using NUnit.Framework;
+//using CiCdInlämning1.Controllers;
+//using CiCdInlämning1.Models.Users;
+//using CiCdInlämning1.Utilities;
+//using NUnit.Framework;
 
 namespace IntegrationTests
 {
-    [TestFixture()]
-    public class Tests
-    {
-        [Test]
-        public void CreateLoginRemoveUser()
-        {
-            //Arrage
+    //[TestFixture()]
+    //public class Tests
+    //{
+    //    [Test]
+    //    public void CreateLoginRemoveUser()
+    //    {
+    //        //Arrage
 
-            User createdUser = new(
-                ReadWrite.ReadLastEmployeeIdFromFile() + 1,
-                "Helge",
-                "Gubbstrutt",
-                "helge@fullgubbe.nu",
-                12000,
-                "Vaktmästare"
-                );
+    //        User createdUser = new(
+    //            ReadWrite.ReadLastEmployeeIdFromFile() + 1,
+    //            "Helge",
+    //            "Gubbstrutt",
+    //            "helge@fullgubbe.nu",
+    //            12000,
+    //            "Vaktmästare"
+    //            );
 
 
-            //Act
-            var loggedInUser = LoginController.Login("admin1", "admin1234");
-            ReadWrite.Serialize(createdUser);
-            ReadWrite.ReadFromFilesAndAddToListOfUsersAndUpdateEmployeeId();
-            var listOfUsers = ReadWrite.GetListOfUsers();
+    //        //Act
+    //        var loggedInUser = LoginController.Login("admin1", "admin1234");
+    //        ReadWrite.Serialize(createdUser);
+    //        ReadWrite.ReadFromFilesAndAddToListOfUsersAndUpdateEmployeeId();
+    //        var listOfUsers = ReadWrite.GetListOfUsers();
 
-            //Assert
-            Assert.Multiple(() =>
-            {
-                Assert.That(loggedInUser, Is.InstanceOf<Account>());
-                Assert.That(listOfUsers, Contains.Item(createdUser));
-            });
-        }
-    }
+    //        //Assert
+    //        Assert.Multiple(() =>
+    //        {
+    //            Assert.That(loggedInUser, Is.InstanceOf<Account>());
+    //            Assert.That(listOfUsers, Contains.Item(createdUser));
+    //        });
+    //    }
+    //}
 }
